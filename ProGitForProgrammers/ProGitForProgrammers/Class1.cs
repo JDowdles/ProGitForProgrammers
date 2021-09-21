c9640f324bc68ba2525b1e4a6e968cba3367dc3b
John Dowdles <0802721@uad.ac.uk>
2021-09-21 16:00:49 +0100

    Created a blank project. Added console write lines to the visual studio project.

---
+++ ProGitForProgrammers/ProGitForProgrammers/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+$
+if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+
+namespace $safeprojectname$
+{
+	public class Class1
+	{
+		Console.WriteLine("Hello World!");
+		Console.WriteLine("I just added this in Visual Studio");
+	}
+}
