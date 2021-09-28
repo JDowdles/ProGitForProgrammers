e7ec39451cadeee43cf15a721a08e4aba83a4a9b
John Dowdles <0802721@uad.ac.uk>
2021-09-28 15:50:37 +0100

    Added Multiply and Divide methods

--- ProGitForProgrammers/ProGitForProgrammers/Calculator.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Calculator.cs
@@ -12,5 +12,17 @@ namespace ProGitForProgrammers
         {
             return left + right;
         }
+        public int Subtract (int left, int right)
+        {
+            return left - right;
+        }
+        public int Multiply(int left, int right)
+        {
+            return left * right;
+        }
+        public int Divide(int left, int right)
+        {
+            return left / right;
+        }
     }
 }
